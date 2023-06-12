542182eb4889e2fd8d94d2eba7203ee376dd6d83
Adhurim4 <adhuriim.muqiqi@gmail.com>
2023-06-12 17:18:05 +0200

    alt

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -35,7 +35,7 @@ const Cryptocurrencies = ({simplified}) => {
           <Link to={`/crypto/${currency.uuid}`}>
             <Card
              title={`${currency.rank}. ${currency.name}`}
-             extra={<img className='crypto-image' src={currency.iconUrl} />}
+             extra={<img alt="crypto-img" className='crypto-image' src={currency.iconUrl} />}
              hoverable
              >
               <p>Price: {millify(currency.price)}</p>
